83aa73ba1cf1cf38e61e8ad9529d38be53f9a042
Simon Fish <si@mon.fish>
2019-03-23 12:17:29 +0000

    Display centered name in container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { Row, Container } from "reactstrap";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <Row>
+        <Container>
+          <h1 className="display-3 text-center">Simon Fish</h1>
+        </Container>
+      </Row>
+    );
   }
 }
 
