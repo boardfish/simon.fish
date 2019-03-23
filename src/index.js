bdd6157262b151168a1f7fc8b6f484167502b94a
Simon Fish <si@mon.fish>
2019-03-23 15:42:32 +0000

    Comment index.js sections
    Future initialisations that get too large for index.js can be explained
    with comments after being extracted and imported

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
+// FontAwesome imports
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faFacebook,
@@ -10,8 +12,10 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
+// Bootstrap and styling imports
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// FontAwesome icon library intialisation
 library.add(faGithub, faEnvelope, faLinkedin, faFacebook, faTwitter);
 
 ReactDOM.render(<App />, document.getElementById("root"));
