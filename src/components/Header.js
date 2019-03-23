00d9732f6dbaa3c18a61d529150be442bb5aa29d
Simon Fish <si@mon.fish>
2019-03-23 13:55:13 +0000

    Reformat social links into btn-group

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,38 +12,35 @@ class Header extends Component {
             <Col className="d-flex flex-column justify-content-center align-items-center">
               <h1 className="display-3">Simon Fish</h1>
               <p className="lead">Student. Software engineer. Explorer.</p>
-              <ul className="fa-ul">
-                <li>
-                  <span className="fa-li">
-                    <FontAwesomeIcon icon={["fab", "github"]} />
-                  </span>
-                  <a href="https://github.com/boardfish">GitHub</a>
-                </li>
-                <li>
-                  <span className="fa-li">
-                    <FontAwesomeIcon icon="envelope" />
-                  </span>
-                  <a href="mailto:si@mon.fish">si@mon.fish</a>
-                </li>
-                <li>
-                  <span className="fa-li">
-                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
-                  </span>
-                  <a href="https://linkedin.com/in/boardfish">LinkedIn</a>
-                </li>
-                <li>
-                  <span className="fa-li">
-                    <FontAwesomeIcon icon={["fab", "twitter"]} />
-                  </span>
-                  <a href="https://twitter.com/boardfish_">Twitter</a>
-                </li>
-                <li>
-                  <span className="fa-li">
-                    <FontAwesomeIcon icon={["fab", "facebook"]} />
-                  </span>
-                  <a href="https://facebook.com/simonfish84">Facebook</a>
-                </li>
-              </ul>
+              <div className="btn-group">
+                <a
+                  href="https://github.com/boardfish"
+                  className="btn btn-primary"
+                >
+                  <FontAwesomeIcon icon={["fab", "github"]} /> GitHub
+                </a>
+                <a href="mailto:si@mon.fish" className="btn btn-primary">
+                  <FontAwesomeIcon icon="envelope" /> si@mon.fish
+                </a>
+                <a
+                  href="https://linkedin.com/in/boardfish"
+                  className="btn btn-primary"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin"]} /> LinkedIn
+                </a>
+                <a
+                  href="https://twitter.com/boardfish"
+                  className="btn btn-primary"
+                >
+                  <FontAwesomeIcon icon={["fab", "twitter"]} /> Twitter
+                </a>
+                <a
+                  href="https://facebook.com/simonfish84"
+                  className="btn btn-primary"
+                >
+                  <FontAwesomeIcon icon={["fab", "facebook"]} /> Facebook
+                </a>
+              </div>
             </Col>
             <Col>
               <img className="w-100 p-3 rounded" src={ProfileImage} />
