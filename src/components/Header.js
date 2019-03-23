fbdd7dad26aad8722d25eb4183a1ecef873d47db
Simon Fish <si@mon.fish>
2019-03-23 13:49:57 +0000

    Add FontAwesome Brands, give social links icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,20 +12,35 @@ class Header extends Component {
             <Col className="d-flex flex-column justify-content-center align-items-center">
               <h1 className="display-3">Simon Fish</h1>
               <p className="lead">Student. Software engineer. Explorer.</p>
-              <ul>
+              <ul className="fa-ul">
                 <li>
+                  <span className="fa-li">
+                    <FontAwesomeIcon icon={["fab", "github"]} />
+                  </span>
                   <a href="https://github.com/boardfish">GitHub</a>
                 </li>
                 <li>
+                  <span className="fa-li">
+                    <FontAwesomeIcon icon="envelope" />
+                  </span>
                   <a href="mailto:si@mon.fish">si@mon.fish</a>
                 </li>
                 <li>
+                  <span className="fa-li">
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                  </span>
                   <a href="https://linkedin.com/in/boardfish">LinkedIn</a>
                 </li>
                 <li>
+                  <span className="fa-li">
+                    <FontAwesomeIcon icon={["fab", "twitter"]} />
+                  </span>
                   <a href="https://twitter.com/boardfish_">Twitter</a>
                 </li>
                 <li>
+                  <span className="fa-li">
+                    <FontAwesomeIcon icon={["fab", "facebook"]} />
+                  </span>
                   <a href="https://facebook.com/simonfish84">Facebook</a>
                 </li>
               </ul>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
+library.add(faGithub, faEnvelope, faLinkedin, faFacebook, faTwitter);
 
 ReactDOM.render(<App />, document.getElementById("root"));
