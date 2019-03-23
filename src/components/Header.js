b00b50e7bd5e60e010c0bd1d10848964721db38f
Simon Fish <si@mon.fish>
2019-03-23 12:49:28 +0000

    Add initial social links

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,23 @@ class Header extends Component {
             <Col className="d-flex flex-column justify-content-center align-items-center">
               <h1 className="display-3">Simon Fish</h1>
               <p className="lead">Student. Software engineer. Explorer.</p>
+              <ul>
+                <li>
+                  <a href="https://github.com/boardfish">GitHub</a>
+                </li>
+                <li>
+                  <a href="mailto:si@mon.fish">si@mon.fish</a>
+                </li>
+                <li>
+                  <a href="https://linkedin.com/in/boardfish">LinkedIn</a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/boardfish_">Twitter</a>
+                </li>
+                <li>
+                  <a href="https://facebook.com/simonfish84">Facebook</a>
+                </li>
+              </ul>
             </Col>
             <Col>
               <img className="w-100 p-3 rounded" src={ProfileImage} />
