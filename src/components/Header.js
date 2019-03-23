64e2bbd55a9199ec965829a2f2c43abbbcbfb0eb
Simon Fish <si@mon.fish>
2019-03-23 12:42:24 +0000

    Add header image, arrange header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Row, Container } from "reactstrap";
+import { Row, Container, Col } from "reactstrap";
+import ProfileImage from "../images/profile.jpg";
 
 class Header extends Component {
   render() {
     return (
       <Row>
-        <Container>
-          <h1 className="display-3 text-center">Simon Fish</h1>
+        <Container style={{ maxHeight: "100vh" }} className="border">
+          <Row>
+            <Col className="d-flex justify-content-center align-items-center">
+              <h1 className="display-3">Simon Fish</h1>
+            </Col>
+            <Col>
+              <img className="w-100" src={ProfileImage} />
+            </Col>
+          </Row>
         </Container>
       </Row>
     );
