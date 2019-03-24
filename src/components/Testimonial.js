92759e635588df12bec5952e8d7f25e539a1a292
Simon Fish <si@mon.fish>
2019-03-24 11:24:01 +0000

    Extract testimonials into components and section

---
+++ src/components/Testimonial.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default props => {
+  return (
+    <blockquote className="blockquote text-center">
+      {props.children}
+      <footer className="blockquote-footer">
+        {props.author}
+        {props.role ? `, ${props.role}` : ""}
+      </footer>
+      <a
+        class="btn btn-lg btn-color btn-outline-primary mt-3"
+        target="_blank"
+        href={props.link}
+      >
+        <FontAwesomeIcon icon={props.icon} /> Full reference
+      </a>
+    </blockquote>
+  );
+};
---
+++ src/components/TestimonialsSection.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Testimonial from "./Testimonial";
+
+export default () => {
+  return (
+    <div>
+      <h2>Testimonials</h2>
+      <Testimonial
+        author="Andy Sykes"
+        role="formerly of Pera Technology"
+        icon="file"
+        link="docs/PeraTechnologyReference.pdf"
+      >
+        <p>
+          "Simon has an ability to take a <b>complex task</b> (both IT-related
+          and non-IT) and not only turn it around extremely quickly, but also to
+          apply a level of thought and <b>ingenuity</b> of someone{" "}
+          <b>a number of years more experienced.</b> It's this level of{" "}
+          <b>output and creativity</b> which made the decision to bring Simon
+          back after a few months in his summer holiday extremely easy."
+        </p>
+      </Testimonial>
+      <Testimonial
+        author="Pauline Narvas"
+        role="Code First Girls course ambassador"
+        icon={["fab", "linkedin"]}
+        link="https://www.linkedin.com/in/boardfish/"
+      >
+        <p>
+          Simon's <b>enthusiasm </b>towards helping out at the Code First: Girls
+          community courses in Sheffield was so admirable. His ability to
+          communicate <b>complex technical concepts </b>to{" "}
+          <b>complete beginners</b> is an important one to have in our
+          increasingly multi-disciplinary world. <br />
+          His genuine passion to <b>help the tech community </b>mixed in with
+          his <b>impressive technical knowledge and skills </b>has made it a
+          pleasure to work with him.
+        </p>
+      </Testimonial>
+    </div>
+  );
+};
