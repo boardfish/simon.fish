99babd1f3e315136a72c8d4f501e7e4de7a6a41e
Simon Fish <sfish@ukcloud.com>
2019-03-26 16:41:47 +0000

    Create expanding portfolio item component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,29 @@ import ContactMe from "./components/ContactMe";
 import PortfolioItem from "./components/PortfolioItem";
 
 class App extends Component {
+  state = { selectedCard: null };
+
+  handleClick(cardName) {
+    if (cardName === this.state.selectedCard) {
+      this.setState({ selectedCard: null });
+    } else {
+      this.setState({ selectedCard: cardName });
+    }
+  }
+
   render() {
     return (
       <Container fluid>
         <Container>
           <Header />
           <About />
+          <div
+            className="d-flex flex-row"
+            style={{
+              flexWrap: "nowrap",
+              overflowX: this.state.selectedCard === null ? "auto" : "visible"
+            }}
+          />
           <TestimonialsSection />
           <ContactMe />
         </Container>
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card, CardTitle, CardText, CardBody } from "reactstrap";
+import posed, { PoseGroup } from "react-pose";
+import styled from "styled-components";
+import { tween } from "popmotion";
 
-export default props => {
-  return (
-    <Card>
-      <CardBody>
-        <CardTitle>
-          <h3>
-            <FontAwesomeIcon icon={props.icon} /> {props.title}
-            <br />
-            <small class="text-muted">{props.location}</small>
-          </h3>
-        </CardTitle>
-      </CardBody>
-      <img width="100%" src={props.image} alt="Card image cap" />
-      <CardBody>
-        <CardText>{props.summary}</CardText>
-      </CardBody>
-    </Card>
-  );
-};
+const Card = styled(
+  posed.div({
+    idle: {
+      height: 500,
+      width: 500,
+      transition: props => tween({ ...props, duration: 250 }),
+      flip: true
+    },
+    fullscreen: {
+      height: "80vh",
+      width: "100vw",
+      transition: props => tween({ ...props, duration: 600 }),
+      flip: true
+    }
+  })
+).attrs({ className: "card" })`
+  transition: flex 0.5s ease-out, opacity 0.1s ease-out,
+    background-color 0.6s ease-out;
+  flex: 1 0 250px;
+`;
+
+class PortfolioItem extends Component {
+  render() {
+    return (
+      <Card
+        pose={this.props.active ? "fullscreen" : "idle"}
+        onClick={this.props.onClick}
+        style={
+          this.props.hidden
+            ? {
+                flex: 0,
+                opacity: 0,
+                whiteSpace: "nowrap"
+              }
+            : { minWidth: 250 }
+        }
+        className={this.props.active ? "bg-dark text-light" : ""}
+      >
+        <div className="card-body row">
+          <div className="card-title col-12">
+            <h4>
+              <FontAwesomeIcon icon={this.props.icon} /> {this.props.title}
+              <br />
+              <small className="text-muted">{this.props.location}</small>
+            </h4>
+          </div>
+          <div className={this.props.active ? "col" : "col-12"}>
+            <img width="100%" src={this.props.image} alt="Card image cap" />
+          </div>
+          <div className={this.props.active ? "col" : "col-12"}>
+            <div className="card-text">
+              {this.props.active ? this.props.description : this.props.summary}
+            </div>
+          </div>
+        </div>
+      </Card>
+    );
+  }
+}
+
+export default PortfolioItem;
