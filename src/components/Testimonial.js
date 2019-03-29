44c2a5133bcaad0afab17e73361af7e71e5425c5
Simon Fish <si@mon.fish>
2019-03-29 12:58:29 +0000

    Load testimonials from file

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, CardBody } from "reactstrap";
+import ReactMarkdown from "react-markdown";
 
 export default props => {
   return (
     <Card className="text-primary">
       <CardBody>
         <blockquote className="blockquote text-center">
-          {props.children}
+          <ReactMarkdown
+            escapeHtml={false}
+            source={props.text}
+            className="card-text"
+          />
           <footer className="blockquote-footer">
             {props.author}
             {props.authorRole ? `, ${props.authorRole}` : ""}
