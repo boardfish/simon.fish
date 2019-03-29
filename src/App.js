efb6fa5747735104f180cd8a3df4330e4b278ef7
Simon Fish <si@mon.fish>
2019-03-29 14:30:09 +0000

    Improve navbar, show tooltip to prompt card expansion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
+import {
+  Container,
+  Col,
+  Row,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledTooltip
+} from "reactstrap";
 import Header from "./components/Header";
 import TestimonialsSection from "./components/TestimonialsSection";
 import About from "./components/About";
 import ContactMe from "./components/ContactMe";
 import PortfolioItem from "./components/PortfolioItem";
 import Portfolio from "./_data/portfolio";
+import Logo from "./assets/images/LogoWhite.svg";
 
 class App extends Component {
   state = { selectedCard: null };
@@ -42,31 +51,77 @@ class App extends Component {
     return (
       <Container fluid>
         <Container>
-          <section className="my-3" id="header">
-            <Header />
-          </section>
-          <section className="my-3" id="about">
-            <About />
-          </section>
-          <section className="my-3" id="portfolio">
-            <h2>Portfolio</h2>
-            <div
-              className="card-group"
-              style={{
-                flexWrap: "nowrap",
-                overflowX: this.state.selectedCard === null ? "auto" : "hidden",
-                overflowY: "hidden"
-              }}
-            >
-              {this.renderPortfolio()}
-            </div>
-          </section>
-          <section className="my-3" id="testimonials">
-            <TestimonialsSection />
-          </section>
-          <section className="my-3" id="contact-me">
-            <ContactMe />
-          </section>
+          <Row>
+            <Col sm={3} md={2} className="bg-dark text-light">
+              <Nav
+                className="text-light flex-column"
+                style={{
+                  position: "fixed",
+                  top: 51
+                }}
+              >
+                <NavItem>
+                  <img
+                    src={Logo}
+                    alt="Pixelated fish logo"
+                    className="mx-auto mb-3"
+                    style={{ width: "10vw" }}
+                  />
+                  <NavLink href="/components/" className="text-light p-0">
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/" className="text-light p-0">
+                    Portfolio
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/" className="text-light p-0">
+                    Testimonials
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/" className="text-light p-0">
+                    Contact Me
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Col>
+            <Col sm={9} md={10} className="bg-primary text-light">
+              <main>
+                <section className="my-3" id="header">
+                  <Header />
+                </section>
+                <section className="my-3" id="about">
+                  <About />
+                </section>
+                <section className="my-3" id="portfolio">
+                  <h2>Portfolio</h2>
+                  <div
+                    className="card-group"
+                    style={{
+                      flexWrap: "nowrap",
+                      overflowX:
+                        this.state.selectedCard === null ? "auto" : "hidden",
+                      overflowY: "hidden"
+                    }}
+                  >
+                    {this.renderPortfolio()}
+                  </div>
+                  <UncontrolledTooltip placement="left" target="portfolio">
+                    Click on an item to learn more.
+                  </UncontrolledTooltip>
+                </section>
+                <section className="my-3" id="testimonials">
+                  <TestimonialsSection />
+                </section>
+                <section className="my-3" id="contact-me">
+                  <ContactMe />
+                </section>
+              </main>
+            </Col>
+          </Row>
         </Container>
       </Container>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,28 @@
 import React, { Component } from "react";
 import { Row, Col, Card, CardBody } from "reactstrap";
 import ProfileImage from "../assets/images/profile.jpg";
-import Logo from "../assets/images/LogoWhite.svg";
 import SocialLinkGroup from "./SocialLinkGroup";
 
 class Header extends Component {
   render() {
     return (
-      <Row>
-        <Col
-          xs="12"
-          lg="6"
-          className="d-flex flex-column justify-content-center align-items-center"
+      <div className="d-flex flex-row">
+        <Card
+          className="text-center text-primary"
+          style={{ flexGrow: 1, flexBasis: 0 }}
         >
-          <Card className="text-center text-primary">
-            <CardBody>
-              <img
-                className="w-100 p-3 rounded"
-                src={ProfileImage}
-                alt="Pixelated fish logo"
-              />
-              <h1 className="display-3">Simon Fish</h1>
-              <p className="lead">Student. Software engineer. Explorer.</p>
-              <SocialLinkGroup />
-            </CardBody>
-          </Card>
-        </Col>
-        <Col
-          xs="12"
-          md="6"
-          className="d-flex justify-content-center align-items-center"
-        >
-          <img
-            className="w-100 p-3 rounded mx-auto"
-            src={Logo}
-            alt="Simon in a GitHub hoodie and Ausdom M05 headphones."
-          />
-        </Col>
-      </Row>
+          <CardBody>
+            <img
+              className="w-100 p-3 rounded"
+              src={ProfileImage}
+              alt="Pixelated fish logo"
+            />
+            <h1 className="display-3">Simon Fish</h1>
+            <p className="lead">Student. Software engineer. Explorer.</p>
+            <SocialLinkGroup />
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
