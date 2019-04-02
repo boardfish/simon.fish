60875e6d078dc243eac4d36fb1c47b3ae5a2d1dd
Simon Fish <sfish@ukcloud.com>
2019-04-02 16:08:14 +0100

    Extract social links to JSON data file

--- src/components/SocialLinkGroup.js
+++ src/components/SocialLinkGroup.js
@@ -1,39 +1,13 @@
 import React from "react";
 import SocialLink from "./SocialLink";
+import socialLinks from "../_data/socialLinks";
 
 export default () => {
   return (
     <div className="btn-group w-100">
-      <SocialLink
-        link="https://github.com/boardfish"
-        colorClass="github"
-        icon={["fab", "github"]}
-        text="GitHub"
-      />
-      <SocialLink
-        link="mailto:si@mon.fish"
-        colorClass="danger"
-        icon="envelope"
-        text="si@mon.fish"
-      />
-      <SocialLink
-        link="https://linkedin.com/in/boardfish"
-        colorClass="linkedin"
-        icon={["fab", "linkedin"]}
-        text="LinkedIn"
-      />
-      <SocialLink
-        link="https://twitter.com/boardfish_"
-        colorClass="twitter"
-        icon={["fab", "twitter"]}
-        text="@boardfish_"
-      />
-      <SocialLink
-        link="https://facebook.com/simonfish84"
-        colorClass="facebook"
-        icon={["fab", "facebook"]}
-        text="Facebook"
-      />
+      {socialLinks.map(socialLink => (
+        <SocialLink {...socialLink} />
+      ))}
     </div>
   );
 };
