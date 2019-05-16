6abc9425d8522c6bc6e6f319e8771a505c3aa847
Simon Fish <sfish@ukcloud.com>
2019-05-16 17:13:14 +0100

    Log in to docker registry

--- build.groovy
+++ build.groovy
@@ -3,6 +3,8 @@ node {
        git credentialsId: 'ssh-key', url: 'git@git.hethi.cc:boardfish/simon.fish-react'
    }
    stage('Run build script') {
-       sh label: '', script: 'scripts/build \$(git tag --points-at HEAD) \$(git rev-parse --abbrev-ref HEAD) \$(git rev-parse --short HEAD)'
-   }
+       withCredentials([usernamePassword(credentialsId: 'docker-registry', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+           sh label: '', script: 'docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} docker.hethi.cc && scripts/build \$(git tag --points-at HEAD) \$(git rev-parse --abbrev-ref HEAD) \$(git rev-parse --short HEAD)'
+       }
+    }
 }
