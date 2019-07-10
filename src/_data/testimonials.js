08c9009b4268f4b74f333facec029218fa8ad59d
Simon Fish <si@mon.fish>
2019-07-10 19:31:31 +0100

    Add placeholder

--- src/_data/testimonials.js
+++ src/_data/testimonials.js
@@ -1,16 +1,25 @@
 export default [
   {
-    author: "Andy Sykes",
-    authorRole: "formerly of Pera Technology",
-    icon: {"prefix": "fas", "iconName": "file"},
-    link: process.env.PUBLIC_URL + "docs/PeraTechnologyReference.pdf",
-    text: "Simon has an ability to take a **complex task** (both IT-related and non-IT) and not only turn it around extremely quickly, but also to apply a level of thought and **ingenuity** of someone **a number of years more experienced.** It's this level of **output and creativity** which made the decision to bring Simon back after a few months in his summer holiday extremely easy."
+    author: "Ben Saunders",
+    authorRole: "Lead Software Engineer, UKCloud Ltd.",
+    icon: { prefix: "fab", iconName: "linkedin" },
+    link: "https://www.linkedin.com/in/boardfish/",
+    text: ""
   },
   {
     author: "Pauline Narvas",
     authorRole: "Code First Girls course ambassador",
-    icon: {"prefix": "fab", "iconName": "linkedin"},
+    icon: { prefix: "fab", iconName: "linkedin" },
     link: "https://www.linkedin.com/in/boardfish/",
-    text: "Simon's **enthusiasm** towards helping out at the Code First: Girls community courses in Sheffield was so admirable. His ability to communicate **complex technical concepts** to **complete beginners** is an important one to have in our increasingly multi-disciplinary world.<br />His genuine passion to **help the tech community** mixed in with his **impressive technical knowledge and skills** has made it a pleasure to work with him."
+    text:
+      "Simon's **enthusiasm** towards helping out at the Code First: Girls community courses in Sheffield was so admirable. His ability to communicate **complex technical concepts** to **complete beginners** is an important one to have in our increasingly multi-disciplinary world.<br />His genuine passion to **help the tech community** mixed in with his **impressive technical knowledge and skills** has made it a pleasure to work with him."
+  },
+  {
+    author: "Andy Sykes",
+    authorRole: "formerly of Pera Technology",
+    icon: { prefix: "fas", iconName: "file" },
+    link: process.env.PUBLIC_URL + "docs/PeraTechnologyReference.pdf",
+    text:
+      "Simon has an ability to take a **complex task** (both IT-related and non-IT) and not only turn it around extremely quickly, but also to apply a level of thought and **ingenuity** of someone **a number of years more experienced.** It's this level of **output and creativity** which made the decision to bring Simon back after a few months in his summer holiday extremely easy."
   }
-]
+];
