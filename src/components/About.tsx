aae3ac6103f993608630bc79749581e6c8e84289
Simon Fish <si@mon.fish>
2019-09-17 13:48:34 +0100

    Update some text content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,13 +7,12 @@ export default () => {
   return (
     <div>
       <p className="text-secondary text-justify">
-        I am a third-year student at the University of Sheffield, studying a
+        I am a final-year student at the University of Sheffield, studying a
         four-year course in Computer Science including a year in industry. My
-        main aim is to become a software developer, with the goal of building
-        things that others will enjoy or find useful, no matter their level of
-        skill. I am passionate about tools and technologies such as Rails and
-        React, and I like to apply myself to personal projects to refine my
-        skills.
+        main aim is to become a software developer, with the goal of teaching
+        and empowering myself and others. I am passionate about tools and
+        technologies such as Rails and React, and I like to apply myself to
+        personal projects to refine my skills.
       </p>
       <Canvas height="12em" zoom={29} />
     </div>
