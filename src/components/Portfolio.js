c5b6881c0ea420c27982309477244a4dcef610bf
Simon Fish <si@mon.fish>
2020-02-12 12:39:20 +0000

    Visual styling for portfolio topic split

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,23 +6,27 @@ import PortfolioMenuItem from "./PortfolioMenuItem";
 import CardBody from "reactstrap/lib/CardBody";
 import Card from "reactstrap/lib/Card";
 import TabContent from "reactstrap/lib/TabContent";
+import ListGroupItemHeading from "reactstrap/lib/ListGroupItemHeading";
 
 export default (selectedCardState, focusCardState) => {
   return (
     <div className="row rounded mx-0" style={{minHeight: "76vh", maxHeight: "76vh"}}>
       <div className="col-3 rounded bg-dark pr-0" style={{ overflowY: "scroll", minHeight: "inherit", maxHeight: "inherit" }}>
-        <ListGroup style={{ minHeight: "inherit" }} className="mt-3 pr-3 d-flex flex-column w-100">
+        <ListGroup style={{ minHeight: "inherit" }} className="mt-3 pr-3 pb-3 d-flex flex-column w-100">
           {Portfolio.map((group, groupIndex) => (
-            group.projects.map((portfolioItem, index) => (
-              <PortfolioMenuItem
-                key={`${groupIndex}-${index}`}
-                id={`${groupIndex}-${index}`}
-                name={portfolioItem.name}
-                icon={portfolioItem.icon}
-                setSelectedCard={selectedCardState[1]}
-                focusCard={focusCardState[1]}
-              />
-            ))
+            <div className="portfolio-menu-group">
+              <ListGroupItemHeading className="mt-2">{group.name}</ListGroupItemHeading>
+              {group.projects.map((portfolioItem, index) => (
+                <PortfolioMenuItem
+                  key={`${groupIndex}-${index}`}
+                  id={`${groupIndex}-${index}`}
+                  name={portfolioItem.name}
+                  icon={portfolioItem.icon}
+                  setSelectedCard={selectedCardState[1]}
+                  focusCard={focusCardState[1]}
+                />
+              ))}
+            </div>
           ))}
         </ListGroup>
       </div>
