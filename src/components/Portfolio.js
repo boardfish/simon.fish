39e9b039daec7d1d523717375ad671d7dc87bd6c
Simon Fish <si@mon.fish>
2020-02-12 12:31:00 +0000

    Update deps, split portfolio section into groups in backend

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,35 +12,36 @@ export default (selectedCardState, focusCardState) => {
     <div className="row rounded mx-0" style={{minHeight: "76vh", maxHeight: "76vh"}}>
       <div className="col-3 rounded bg-dark pr-0" style={{ overflowY: "scroll", minHeight: "inherit", maxHeight: "inherit" }}>
         <ListGroup style={{ minHeight: "inherit" }} className="mt-3 pr-3 d-flex flex-column w-100">
-          {Portfolio.map((portfolioItem, key) => {
-            return (
+          {Portfolio.map((group, groupIndex) => (
+            group.projects.map((portfolioItem, index) => (
               <PortfolioMenuItem
-                key={key}
-                id={key}
+                key={`${groupIndex}-${index}`}
+                id={`${groupIndex}-${index}`}
                 name={portfolioItem.name}
                 icon={portfolioItem.icon}
                 setSelectedCard={selectedCardState[1]}
                 focusCard={focusCardState[1]}
               />
-            );
-          })}
+            ))
+          ))}
         </ListGroup>
       </div>
       <TabContent className="col-9 px-0" activeTab={selectedCardState[0]} style={{overflowY: "scroll", minHeight: "inherit", maxHeight: "inherit"}}>
         <Card className="bg-dark" style={{minHeight: "inherit"}}>
           <CardBody className="d-flex flex-column justify-content-center">
-            {Portfolio.map((portfolioItem, index) => {
-              return (
+            {Portfolio.map((group, groupIndex) => (
+              group.projects.map((portfolioItem, index) => (
                 <PortfolioItem
-                  {...portfolioItem}
-                  key={index}
-                  id={index}
-                  active={selectedCardState[0] === index}
-                  focused={focusCardState[0]}
-                  focusCard={focusCardState[1]}
-                />
-              );
-            })}
+                    {...portfolioItem}
+                    key={`${groupIndex}-${index}`}
+                    id={`${groupIndex}-${index}`}
+                    active={selectedCardState[0] === `${groupIndex}-${index}`}
+                    focused={focusCardState[0]}
+                    focusCard={focusCardState[1]}
+                  />
+                ))
+              )
+            )}
           </CardBody>
         </Card>
       </TabContent>
